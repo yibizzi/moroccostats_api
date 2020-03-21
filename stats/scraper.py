89b29c39eedc1774de380b3de73a693d91cbadc5
yibizzi <ibizzi.younes@ine.inpt.ma>
2020-03-21 18:26:43 +0100

    dbugging

--- stats/scraper.py
+++ stats/scraper.py
@@ -124,11 +124,12 @@ def get_regions_morocco(heads):
     url = 'http://www.covidmaroc.ma/'
     
     response = requests.get(url , headers=heads)
-    err =  {'error': str(response)}
     print('headers: \n' + str(response.headers))
     sys.stdout.flush()
     
     response=response.text
+    err =  {'error': str(response)}
+    
     soup = BeautifulSoup(response, "html.parser").select("table")
     try:
         Soup = soup[0]
