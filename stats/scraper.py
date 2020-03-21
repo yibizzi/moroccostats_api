da9af810983ddce6ad68a3f3302b03f6c56580fe
yibizzi <ibizzi.younes@ine.inpt.ma>
2020-03-21 18:09:17 +0100

    dbugging

--- stats/scraper.py
+++ stats/scraper.py
@@ -122,11 +122,15 @@ def get_regions_morocco():
 
 
     url = 'http://www.covidmaroc.ma/'
-    response = requests.get(url , headers={
-        'X-FRAME-OPTIONS': 'SAMEORIGIN',
+    heads = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'referrer': 'https://google.co.ma',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Pragma': 'no-cache',
         'Connection':'close',
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-        })
+    }
+    response = requests.get(url , headers=heads)
     
     print('headers: \n' + str(response.headers))
     response=response.text
